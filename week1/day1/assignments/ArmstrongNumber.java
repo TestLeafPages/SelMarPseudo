8a989f0123b298dd98627c8286bc201d400ee89c
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-04-06 12:55:04 +0530

    Update ArmstrongNumber.java

--- week1/day1/assignments/ArmstrongNumber.java
+++ week1/day1/assignments/ArmstrongNumber.java
@@ -36,71 +36,29 @@ public class ArmstrongNumber {
 
 	 * 3) 
 
-	 * 
-
 	 */
-
-	
-
 	public static void main(String[] args) {
-
-		
 
 		// Declare your input
 
-		
-
-		
-
 		// Declare 3 more int variables as calculated, remainder, original (Tip: Initialize calculated alone)
-
-		
-		
 
 		// Assign input into variable original 
 
-		
-
-		
-
 		// Use loop to calculate: which loop to use until the number greater than 0??
 
-		
+		// Within loop: get the remainder when done by 10 (Tip: Use Mod operator)
 
-			// Within loop: get the remainder when done by 10 (Tip: Use Mod operator)
+		// Within loop: get the quotient when done by 10 (Tip: Assign the result to input)
 
-			
-
-			// Within loop: get the quotient when done by 10 (Tip: Assign the result to input)
-
-			
-
-			// Within loop: Add calculated with the cube of remainder & assign it to calculated
-
-			
-		
-
-		
+		// Within loop: Add calculated with the cube of remainder & assign it to calculated
 
 		// Check whether calculated and original are same
-
 		
-
-			//When it matches print it as Armstrong number
+		//When it matches print it as Armstrong number
 
 
 		
-
-
-
-		
-
-		
-
-		
-
-		
-
 
 
 	}
